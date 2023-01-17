f189b42258b686120e38670c0b4803acbefdffda
卷心菜汪 <rv_jiang@qq.com>
2023-01-17 12:36:08 +0800

    feat(NotFound): add-notfound-page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Result, Button } from "antd";
+import { useNavigate } from "@umijs/max";
 
-function NotFoundPage() {
-  return <div>NotFoundPage</div>;
-}
+const NotFoundPage: React.FC = () => {
+  const navigate = useNavigate();
+  const backHome = () => {
+    navigate("/home");
+  };
+
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="抱歉，您访问的页面不存在"
+      extra={
+        <Button onClick={backHome} type="primary">
+          返回首页
+        </Button>
+      }
+    />
+  );
+};
 
 export default NotFoundPage;
