6815054f32d40e7bb63ba2e925d090cd1cb4f02a
卷心菜汪 <rv_jiang@qq.com>
2023-01-17 11:17:28 +0800

    feat(config.ts): deployment-configuration

--- config/config.ts
+++ config/config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from "@umijs/max";
 import routes from "./routes";
 
 export default defineConfig({
+  /* 打包配置 */
+  // base需为服务器分配的前置路由名称
+  base: "/umi-cms/",
+  publicPath: "./",
+
+  /* 插件模块 */
   antd: {},
   access: {},
   model: {},
