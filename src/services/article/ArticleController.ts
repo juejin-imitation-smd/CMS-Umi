ae25d073276608fc59846624c5064feaba320bd4
Wei <939761636@qq.com>
2023-01-25 19:24:50 +0800

    feat(service): 新增文章管理对应接口

---
+++ src/services/article/ArticleController.ts
@@ -0,0 +1,70 @@
+import { request } from "@umijs/max";
+// 获取文章列表
+export async function queryArticleList(
+  params: {
+    page: number;
+    size: number;
+  },
+  options?: { [key: string]: any },
+) {
+  return request<ArticleAPI.Result_PageInfo_ArticleInfo>(
+    "/api/cms/getArticles",
+    {
+      method: "GET",
+      params: { ...params },
+      ...(options || {}),
+    },
+  );
+}
+
+// 获取指定文章内容
+export async function getArticleDetail(
+  params: {
+    id: number;
+  },
+  options?: { [key: string]: any },
+) {
+  return request<ArticleAPI.Result_PageInfo_ArticleInfo>("/api/cms/article", {
+    method: "GET",
+    params: { ...params },
+    ...(options || {}),
+  });
+}
+
+// 新建文章
+export async function addArticle(
+  body: ArticleAPI.ArticleInfoVO,
+  options?: { [key: string]: any },
+) {
+  return request<ArticleAPI.Result_PageInfo_ArticleInfo>("/api/cms/article", {
+    method: "POST",
+    data: body,
+    ...(options || {}),
+  });
+}
+
+// 修改文章
+export async function modifyArticle(
+  body: ArticleAPI.ArticleInfo,
+  options?: { [key: string]: any },
+) {
+  return request<ArticleAPI.Result_PageInfo_ArticleInfo>("/api/cms/article", {
+    method: "PUT",
+    data: body,
+    ...(options || {}),
+  });
+}
+
+// 删除文章
+export async function deleteArticle(
+  params: {
+    id: number;
+  },
+  options?: { [key: string]: any },
+) {
+  return request<ArticleAPI.Result_PageInfo_ArticleInfo>("/api/cms/article", {
+    method: "DELETE",
+    params: { ...params },
+    ...(options || {}),
+  });
+}
