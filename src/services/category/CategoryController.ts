f856ca46fc6f823d8739d21d14c46f67d8a18c13
卷心菜汪 <rv_jiang@qq.com>
2023-01-27 19:35:38 +0800

    feat(CategoryAPI): perfect category interface and type

--- src/services/category/CategoryController.ts
+++ src/services/category/CategoryController.ts
@@ -1,20 +1,65 @@
 import { request } from "@umijs/max";
 
+/**
+ * @Description: 分页查询
+ */
 export async function queryCategoryList(
-  params: {
-    size: number;
-    page: number;
-  },
+  params: CategoryAPI.Request_PageInfo_CategoryInfo,
   options?: { [key: string]: any },
 ) {
-  return request<CategoryAPI.Result_PageInfo_CategoryInfo__>(
-    "/api/cms/column",
-    {
-      method: "GET",
-      params: {
-        ...params,
-      },
-      ...(options || {}),
+  return request<CategoryAPI.Result_PageInfo_CategoryInfo>("/api/cms/column", {
+    method: "GET",
+    params: {
+      ...params,
     },
-  );
+    ...(options || {}),
+  });
+}
+
+/**
+ * @Description: 新增类别
+ */
+export async function addCategory(
+  params: CategoryAPI.Request_AddCategory,
+  options?: { [key: string]: any },
+) {
+  return request<CategoryAPI.Result_AddCategoy>("/api/cms/column", {
+    method: "POST",
+    data: {
+      ...params,
+    },
+    ...(options || {}),
+  });
+}
+
+/**
+ * @Description: 修改类别
+ */
+export async function modifyCategory(
+  params: CategoryAPI.Request_ModifyCategory,
+  options?: { [key: string]: any },
+) {
+  return request<CategoryAPI.Result_ModifyCategory>("/api/cms/column", {
+    method: "PUT",
+    data: {
+      ...params,
+    },
+    ...(options || {}),
+  });
+}
+
+/**
+ * @Description: 删除类别
+ */
+export async function deleteCategory(
+  params: CategoryAPI.Request_DeleteCategory,
+  options?: { [key: string]: any },
+) {
+  return request<CategoryAPI.Result_DeleteCategory>("/api/cms/column", {
+    method: "DELETE",
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  });
 }
--- src/services/category/typings.d.ts
+++ src/services/category/typings.d.ts
@@ -11,6 +11,7 @@ declare namespace CategoryAPI {
   interface TagInfo {
     id: number;
     label: string;
+    category?: CategoryInfo;
   }
 
   /**
@@ -22,13 +23,37 @@ declare namespace CategoryAPI {
     data: T;
   }
 
-  // 分页查询
+  /* 分页查询 */
   interface PageInfo_CategoryInfo__ {
     list: CategoryInfo[];
     total: number;
   }
-  /**
-   * @Description: 分页查询响应结果
-   */
-  type Result_PageInfo_CategoryInfo__ = Result__<PageInfo_CategoryInfo__>;
+  type Request_PageInfo_CategoryInfo = {
+    page: number;
+    size: number;
+  };
+  type Result_PageInfo_CategoryInfo = Result__<PageInfo_CategoryInfo__>;
+
+  /* 新增类别 */
+  type Request_AddCategory = {
+    name: string;
+    url: string;
+    labels: string[];
+  };
+  type Result_AddCategoy = Result__<{ id: string }>;
+
+  /* 修改类别 */
+  type Request_ModifyCategory = {
+    id: number;
+    name: string;
+    url: string;
+    labels: string[];
+  };
+  type Result_ModifyCategory = Result__<null>;
+
+  /* 删除类别 */
+  type Request_DeleteCategory = {
+    id: string;
+  };
+  type Result_DeleteCategory = Result__<null>;
 }
