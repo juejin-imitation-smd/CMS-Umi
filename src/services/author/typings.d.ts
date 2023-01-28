2a959e6318f07ce25af01b45feb8c6f497416129
卷心菜汪 <rv_jiang@qq.com>
2023-01-28 13:40:47 +0800

    feat(AuthorAPI): perfect author interface and type

--- src/services/author/typings.d.ts
+++ src/services/author/typings.d.ts
@@ -1,18 +1,57 @@
 declare namespace AuthorAPI {
+  // 作者数据结构
   interface AuthorInfo {
-    uid: string;
+    id: number;
     usernme: string;
     description: string;
     avatar: string;
     article_count: number;
   }
 
-  interface Result_PageInfo_AuthorInfo__ {
+  /**
+   * @Description: 响应结果封装
+   */
+  interface Result__<T> {
+    code: number;
+    msg: string;
+    data: T;
+  }
+
+  /* 分页查询 */
+  interface PageInfo_AuthorInfo__ {
+    list: AuthorInfo[];
+    total: number;
+  }
+  type Request_PageInfo_AuthorInfo = {
+    size: number;
+    page: number;
+    username?: string;
+  };
+  type Result_PageInfo_AuthorInfo = {
     code: number;
     msg: string;
     data: {
       total: number;
       list: AuthorInfo[];
     };
-  }
+  };
+
+  /* 新增作者 */
+  type Request_AddAuthor = {
+    username: string;
+    description: string;
+    avatar: string;
+    article_count: string;
+  };
+  type Result_AddAuthor = Result__<{ id: number }>;
+
+  /* 修改作者 */
+  type Request_ModifyAuthor = AuthorInfo;
+  type Result_ModifyAuthor = Result__<null>;
+
+  /* 删除作者 */
+  type Request_DeleteAuthor = {
+    id: number;
+  };
+  type Result_DeleteAuthor = Result__<null>;
 }
