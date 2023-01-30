e9a1db3678bcec0f57e57e8ade5596fc1b4d16de
Wei <939761636@qq.com>
2023-01-30 20:10:46 +0800

    feat(*): 新增markdown编辑器添加图片功能

--- src/components/MdRender/MdEditor/index.tsx
+++ src/components/MdRender/MdEditor/index.tsx
@@ -1,12 +1,22 @@
 import { BytemdPlugin } from "bytemd";
 import frontmatter from "@bytemd/plugin-frontmatter";
+import image from "../plugin-image";
 import gemoji from "@bytemd/plugin-gemoji";
+import gfm from "@bytemd/plugin-gfm";
 import theme from "../plugin-theme";
+import mermaid from "@bytemd/plugin-mermaid";
 import { Editor } from "@bytemd/react";
 
 import zhHans from "bytemd/locales/zh_Hans.json";
 
-const plugins: Array<BytemdPlugin> = [frontmatter(), gemoji(), theme()];
+const plugins: Array<BytemdPlugin> = [
+  frontmatter(),
+  gemoji(),
+  image(),
+  gfm(),
+  theme(),
+  mermaid(),
+];
 
 interface Props {
   value: string;
