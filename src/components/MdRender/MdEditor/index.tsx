1c88de4a89304a5fd35f70f20fb8c030ea55f77b
Wei <939761636@qq.com>
2023-01-30 20:19:27 +0800

    feat(*): 添加bytemd剩余所有插件

--- src/components/MdRender/MdEditor/index.tsx
+++ src/components/MdRender/MdEditor/index.tsx
@@ -1,21 +1,27 @@
 import { BytemdPlugin } from "bytemd";
+import breaks from "@bytemd/plugin-breaks";
 import frontmatter from "@bytemd/plugin-frontmatter";
 import image from "../plugin-image";
 import gemoji from "@bytemd/plugin-gemoji";
 import gfm from "@bytemd/plugin-gfm";
+import math from "@bytemd/plugin-math";
 import theme from "../plugin-theme";
 import mermaid from "@bytemd/plugin-mermaid";
+import mediumZoom from "@bytemd/plugin-medium-zoom";
 import { Editor } from "@bytemd/react";
 
 import zhHans from "bytemd/locales/zh_Hans.json";
 
 const plugins: Array<BytemdPlugin> = [
+  breaks(),
   frontmatter(),
   gemoji(),
   image(),
   gfm(),
+  math(),
   theme(),
   mermaid(),
+  mediumZoom(),
 ];
 
 interface Props {
